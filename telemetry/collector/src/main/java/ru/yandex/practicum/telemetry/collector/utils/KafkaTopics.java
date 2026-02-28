6a5cd76a9ab8ea46b234c68b4ef2b0a6759f86ff
n20va <99407852+n20va@users.noreply.github.com>
2026-02-28 10:40:30 +0300

    Delete telemetry directory

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/utils/KafkaTopics.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.telemetry.collector.utils;
-
-public interface KafkaTopics {
-    String HUBS_EVENTS = "telemetry.hubs.v1";
-    String SENSORS_EVENTS = "telemetry.sensors.v1";
-    String SNAPSHOTS_EVENTS = "telemetry.snapshots.v1";
-}

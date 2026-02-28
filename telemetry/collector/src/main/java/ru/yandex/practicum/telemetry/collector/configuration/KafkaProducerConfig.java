401ab5ace716cf17679b05968e902953bf4be15e
n20va <99407852+n20va@users.noreply.github.com>
2026-02-28 10:54:09 +0300

    Update KafkaProducerConfig to use bootstrap servers
    Refactor KafkaProducerConfig to use bootstrap servers from properties.

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/configuration/KafkaProducerConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/configuration/KafkaProducerConfig.java
@@ -1,38 +1,20 @@
-package ru.yandex.practicum.telemetry.collector.configuration;
-
-import lombok.RequiredArgsConstructor;
-import org.apache.avro.specific.SpecificRecordBase;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import ru.yandex.practicum.serialization.avro.GeneralAvroSerializer;
-
-import java.util.Properties;
-
 @Configuration
-@RequiredArgsConstructor
 @EnableConfigurationProperties(KafkaProperties.class)
+@RequiredArgsConstructor
 public class KafkaProducerConfig {
 
     private final KafkaProperties kafkaProperties;
-    private final org.springframework.boot.autoconfigure.kafka.KafkaProperties springKafkaProperties;
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean(destroyMethod = "close")
     public Producer<String, SpecificRecordBase> kafkaProducer() {
         Properties props = new Properties();
-
-        // Подтягиваем базовые настройки из spring.kafka.* (включая bootstrap-servers)
-        props.putAll(springKafkaProperties.buildProducerProperties());
-
-        // Явно задаём сериализаторы (можно и через spring.kafka.producer.*, но так у тебя было изначально)
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
 
-        // Параметры из collector.kafka.*
         props.put(ProducerConfig.RETRIES_CONFIG, kafkaProperties.getRetries());
         props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, kafkaProperties.getDeliveryTimeoutMs());
         props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, kafkaProperties.getRequestTimeoutMs());

877d53e5644257fd7411bad5a9b45c050f973180
n20va <99407852+n20va@users.noreply.github.com>
2026-03-02 20:45:55 +0300

    Delete telemetry/analyzer directory

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/repository/ActionRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.analyzer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.yandex.practicum.analyzer.model.Action;
-
-public interface ActionRepository extends JpaRepository<Action, Long> {
-}

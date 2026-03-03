7a1329b98933fb35c7b749659eb48ffc8cfa87d1
n20va <99407852+n20va@users.noreply.github.com>
2026-03-03 18:41:12 +0300

    Delete telemetry directory

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/model/ActionType.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.analyzer.model;
-
-public enum ActionType {
-    ACTIVATE,
-    DEACTIVATE,
-    INVERSE,
-    SET_VALUE
-}
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/repository/ConditionRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.analyzer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.yandex.practicum.analyzer.model.Condition;
-
-public interface ConditionRepository extends JpaRepository<Condition, Long> {
-}
